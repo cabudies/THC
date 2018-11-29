82b95b6295e732254c3e88b427c0605b31e0e468
Gurjas Singh <gurjassingh92@gmail.com>
2018-11-29 14:06:59 +0530

    1. Mobile Authentication. 2. User verification.

--- app/src/main/java/com/example/asus/thc/AccountActivity/HomePage.java
+++ app/src/main/java/com/example/asus/thc/AccountActivity/HomePage.java
@@ -1,47 +1,86 @@
 package com.example.asus.thc.AccountActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.asus.thc.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomePage extends AppCompatActivity {
 
     Button btnHomeLogin,btnLocation;
-    TextView txtSlogan;
+    TextView userSignedIn;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_page);
+        setContentView(R.layout.home_screen);
 
-        btnHomeLogin=(Button)findViewById(R.id.btnhomelogin);
-        btnLocation=(Button)findViewById(R.id.btnLocation);
+        userSignedIn = findViewById(R.id.home_screen_text_view_user);
+        firebaseAuth = FirebaseAuth.getInstance();
+//        btnHomeLogin = findViewById(R.id.btnhomelogin);
+//        btnLocation = findViewById(R.id.btnLocation);
+//
+//        txtSlogan = findViewById(R.id.txtSlogan);
+//        firebaseAuth = FirebaseAuth.getInstance();
+//        Typeface face = Typeface.createFromAsset(getAssets(),"fonts/Fh_Ink.ttf");
+//        txtSlogan.setTypeface(face);
+//
+//        btnHomeLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent signIn = new Intent(HomePage.this, SignInForm.class);
+//                startActivity(signIn);
+//            }
+//        });
+//
+//        btnLocation.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
 
-        txtSlogan=(TextView)findViewById(R.id.txtSlogan);
-        Typeface face = Typeface.createFromAsset(getAssets(),"fonts/Fh_Ink.ttf");
-        txtSlogan.setTypeface(face);
+    }
 
-        btnHomeLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent signIn = new Intent(HomePage.this, SignInForm.class);
-                startActivity(signIn);
-            }
-        });
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-        btnLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
 
-            }
-        });
+        if (currentUser == null) {
+            // show alert dialog box to user to such that no user found. Please
+            // show new user.
+            userSignedIn.setText("No user found.");
+            AlertDialog.Builder builder = new AlertDialog.Builder(HomePage.this);
+            builder.setTitle("No user found.");
+            builder.setMessage("Please login to create a new account.");
+            builder.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(HomePage.this, SignInForm.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    dialog.dismiss();
+                }
+            });
 
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        } else {
+            // fetch the records from firestore
+            userSignedIn.setText("User successfully Signed In");
+        }
     }
 }
--- app/src/main/java/com/example/asus/thc/AccountActivity/SignInForm.java
+++ app/src/main/java/com/example/asus/thc/AccountActivity/SignInForm.java
@@ -1,114 +1,146 @@
 package com.example.asus.thc.AccountActivity;
 
-import android.app.ProgressDialog;
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.ProgressBar;
 
-import com.example.asus.thc.Model.User;
 import com.example.asus.thc.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
-import com.google.firebase.FirebaseTooManyRequestsException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.concurrent.TimeUnit;
 
-public class SignInForm extends AppCompatActivity {
+
+public class SignInForm extends AppCompatActivity implements DialogInterface.OnClickListener {
 
     private EditText edtPhone;
-    private EditText edtPassword;
     private Button loginBtn;
-    private Button signupBtn;
-
-    DatabaseReference table_user;
+    private static final String TAG = SignInForm.class.getName();
+    private ProgressBar signInProgressBar;
+    private FirebaseAuth firebaseAuth;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in_form);
 
-        edtPhone=(EditText)findViewById(R.id.edtPhone);
-        edtPassword=(EditText)findViewById(R.id.edtPassword);
-        loginBtn=(Button)findViewById(R.id.loginBtn);
-        signupBtn=(Button)findViewById(R.id.signupBtn);
+        edtPhone = findViewById(R.id.edtPhone);
+        loginBtn = findViewById(R.id.loginBtn);
+        signInProgressBar = findViewById(R.id.sign_in_progress);
 
-        //INIT FIREBASE
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        table_user = FirebaseDatabase.getInstance().getReference("User");
-
-        loginBtn.setOnClickListener(new View.OnClickListener() {
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
             @Override
-            public void onClick(View v) {
+            public void onVerificationCompleted(final PhoneAuthCredential phoneAuthCredential) {
+                Log.d(TAG, "Verification completed.");
 
-                final ProgressDialog mDialog = new ProgressDialog(SignInForm.this);
-                mDialog.setMessage("Please waiting...");
-                mDialog.show();
-
-                table_user.child("User").addValueEventListener(new ValueEventListener(){
-
-
+                signInProgressBar.setVisibility(View.GONE);
+                AlertDialog.Builder builder = new AlertDialog.Builder(SignInForm.this);
+                builder.setTitle("Verification Completed.");
+                builder.setMessage("Signing in the user.");
+                edtPhone.setText("");
+                builder.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
                     @Override
-                    public void onDataChange(DataSnapshot dataSnapshot){
-                        //Check if user does not exist in database
-                        if(dataSnapshot.child(edtPhone.getText().toString()).exists()){
-                            //Get User Information
-                            mDialog.dismiss();
-                            User user = dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);
-                            if (User.getPassword().equals(edtPassword.getText().toString())) {
-                                Toast.makeText(SignInForm.this, "Sign in successfully !", Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(SignInForm.this, "Sign in failed !", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                        else{
-                            mDialog.dismiss();
-                            Toast.makeText(SignInForm.this, "User does not exist. Please sign up!", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                    @Override
-                    public void onCancelled(DatabaseError databaseError){
-
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        signInProgressBar.setVisibility(View.VISIBLE);
+                        signInUser(phoneAuthCredential);
                     }
                 });
 
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+                Log.d(TAG, "Verification failed");
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(SignInForm.this);
+                builder.setTitle("Verification Failed.");
+                builder.setMessage("Please try again.");
+                edtPhone.setText("");
+                builder.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        };
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = "+91" + edtPhone.getText().toString();
+                PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                        phoneNumber,
+                        60,
+                        TimeUnit.SECONDS,
+                        SignInForm.this,
+                        mCallbacks);
+
+                signInProgressBar.setVisibility(View.VISIBLE);
             }
         });
 
-        signupBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(
-                        SignInForm.this,
-                            SignUpForm.class);
-                    startActivity(intent);
-                }
-
-        });
-
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-
-                                        }
-        });
     }
 
+    private void signInUser(PhoneAuthCredential phoneAuthCredential) {
+        firebaseAuth.signInWithCredential(phoneAuthCredential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        signInProgressBar.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "User signed in Successfully.");
+                            Intent intent = new Intent(SignInForm.this, HomePage.class);
+                            startActivity(intent);
+                        } else {
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                Log.e(TAG, "Signing in failed. Please try again");
+                                AlertDialog.Builder builder = new AlertDialog.Builder(SignInForm.this);
+                                builder.setTitle("No user found.");
+                                builder.setMessage("Please click on 'Login' button to create new user account");
+                                builder.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+
+                                AlertDialog alertDialog = builder.create();
+                                alertDialog.show();
+                            }
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        // dismiss the dialog interface.
+        dialog.dismiss();
+    }
 }
